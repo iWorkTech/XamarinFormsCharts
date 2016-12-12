6721032aa4aca2a23f695d51a3a12a3d564051fb
vaishali.l <vaishali.l@SI-DEV-17>
2016-12-12 16:23:48 +0530

    Test5

---
+++ Charts/Charts/Charts/InternalsVisisbleTo.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Charts.Droid")]
+[assembly: InternalsVisibleTo("Charts.iOS")]
+[assembly: InternalsVisibleTo("Charts.WinRT")]
+[assembly: InternalsVisibleTo("Charts.WinRT.Phone")]
+[assembly: InternalsVisibleTo("Charts.UWP")]
---
+++ Charts/Charts/Charts/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Charts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+    }
+}
