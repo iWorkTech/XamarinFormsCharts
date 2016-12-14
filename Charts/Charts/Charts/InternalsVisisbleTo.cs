4879a15544a31ab4f3bcdb758549754e22e41cd4
vaishali.l <vaishali.l@SI-DEV-17>
2016-12-14 13:04:00 +0530

    DelREq1

--- Charts/Charts/Charts/InternalsVisisbleTo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Charts.Droid")]
-[assembly: InternalsVisibleTo("Charts.iOS")]
-[assembly: InternalsVisibleTo("Charts.WinRT")]
-[assembly: InternalsVisibleTo("Charts.WinRT.Phone")]
-[assembly: InternalsVisibleTo("Charts.UWP")]
