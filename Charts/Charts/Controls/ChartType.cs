70e5ee1b8406a2276e645fa6c5dda406bc8b981e
vaishali.l <vaishali.l@SI-DEV-17>
2016-12-14 13:05:55 +0530

    NewData

---
+++ Charts/Charts/Controls/ChartType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charts.Controls
+{
+    public enum ChartType
+    {
+        /// <summary>
+        /// The bar
+        /// </summary>
+        Bar = 0,
+        /// <summary>
+        /// The line
+        /// </summary>
+        Line = 1,
+        /// <summary>
+        /// The pie
+        /// </summary>
+        Pie = 2
+    }
+}
