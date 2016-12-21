205c7b36f533919a8ac91dbc2cbf3ec46bc53a42
SI-DEV-18\ashwini.h <vijay.p@iworktech.com>
2016-12-21 13:06:21 +0530

    XML cum CS

---
+++ Charts/Charts/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Charts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            XAML.Clicked += XAML_Clicked;
+            CSharp.Clicked += CSharp_Clicked;
+        }
+        private async void CSharp_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Barchart());
+        }
+
+        private async void XAML_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BarChartPage());
+        }
+    }
+}
